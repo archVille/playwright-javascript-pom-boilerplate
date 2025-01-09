b0c59e3d5f3da950095b46693323f7258e17ca33
panagiotis.meretis@wealthwizards.com <pmeretis@gmail.com>
2025-01-09 19:31:39 +0000

    comment out code that has been checked in wrongly

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await expect(page.getByLabel('Συναίνεση', { exact: true }).getByRole('paragraph')).toContainText('Συναίνεση');
-  await expect(page.getByLabel('Δεν συγκατατίθεμαι')).toBeVisible();
-  await expect(page.getByLabel('Συναίνεση', { exact: true })).toBeVisible();
-  await page.getByLabel('Συναίνεση', { exact: true }).click();
-  await page.getByRole('button', { name: 'ΟΧΙ ΤΩΡΑ' }).click();
+  // await expect(page.getByLabel('Συναίνεση', { exact: true }).getByRole('paragraph')).toContainText('Συναίνεση');
+  // await expect(page.getByLabel('Δεν συγκατατίθεμαι')).toBeVisible();
+  // await expect(page.getByLabel('Συναίνεση', { exact: true })).toBeVisible();
+  // await page.getByLabel('Συναίνεση', { exact: true }).click();
+  // await page.getByRole('button', { name: 'ΟΧΙ ΤΩΡΑ' }).click();
 });
